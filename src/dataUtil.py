c5784cc23ebed3b6775324426440e2c282805c3d
Charles Burlin <cburlin@stanford.edu>
2017-11-11 23:08:47 -0800

    OOP design for MusicPattern works

--- src/dataUtil.py
+++ src/dataUtil.py
@@ -1,100 +1,106 @@
 import midi
-"""
-# Instantiate a MIDI Pattern (contains a list of tracks)
-pattern = midi.Pattern()
-# Instantiate a MIDI Track (contains a list of MIDI events)
-track = midi.Track()
-# Append the track to the pattern
-pattern.append(track)
-# Instantiate a MIDI note on event, append it to the track
-on = midi.NoteOnEvent(tick=0, velocity=20, pitch=midi.G_3)
-track.append(on)
-# Instantiate a MIDI note off event, append it to the track
-off = midi.NoteOffEvent(tick=100, pitch=midi.G_3)
-track.append(off)
-# Add the end of track event, append it to the track
-eot = midi.EndOfTrackEvent(tick=1)
-track.append(eot)
-# Print out the pattern
-print pattern
-# Save the pattern to disk
-midi.write_midifile("example.mid", pattern)
-"""
+import os.path
 
-class BasicTrack():
+class MusicPattern():
     """
-    This class represents a muscial track and provides
-    method to convert it to a midi format
+    This class represents a musical pattern and provides
+    method to convert it across midi, lisp and prim format
     """
-    def Lisp_to_Midi(pattern_in, tracks_lisp):
-        pattern_out = midi.containers.Pattern() # Create new pattern
-        pattern_out.format = pattern_in.format # Copy format from original MIDI file
-        pattern_out.resolution = pattern_in.resolution # Copy resolution from original MIDI file
-        pattern_out.insert(2, pattern_in[0]) # Copy track[0] of Metadata from original MIDI file
+    def __init__(self, midiFile=None):
+        if midiFile is not None:
+            self.read(midiFile)
 
-        # Create as many tracks as original MIDI file in new pattern
-        for i, track_in in enumerate(pattern_in):
-            if i > 0:
-                new_track = midi.containers.Track()
-                pattern_out.insert(2, new_track)
+    def read(self, midiFile):
+        self.midiFile    = midiFile
+        self.midiPattern = midi.read_midifile(midiFile)
+        self.numTracks   = len(self.midiPattern)-1
+        self.lispTracks  = []
 
-        # Copy metadata from melodic tracks inside the melodic tracks of the new pattern
-        End_index = []
-        for i, track in enumerate(pattern_in):
-            if i > 0:
-                index_note = 0
-                for j, event in enumerate(track):
-                    if type(event) not in [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
-                        pattern_out[i].append(event)
+        for midiTrackId, midiTrack in enumerate(self.midiPattern):
+            if midiTrackId == 0: continue
+
+            lispTrack = []
+            for eventNum, event in enumerate(midiTrack):
+                if type(event)                 == midi.events.NoteOnEvent and \
+                   type(midiTrack[eventNum+1]) == midi.events.NoteOffEvent:
+                    # If not a silence, add (pitch, dur)
+                    if event.tick == 0:
+                        pitch  = event.data[0]
+                        dur    = midiTrack[eventNum+1].tick - event.tick
+                        notes  = [(pitch, dur)]
+
+                    # Otherwise add silence and next note
                     else:
-                        index_note = j
-                End_index.append(len(track) - index_note - 1) # keeps the meta of the tracks in the new pattern at the same place as the original MIDI file
+                        notes = [("silence", event.tick)]
+                        pitch = midiTrack[eventNum+1].data[0]
+                        dur   = midiTrack[eventNum+1].tick
+                        notes += [(pitch,duration)]
 
-        # Create new events inside each melodic track of new pattern from lisp
-        for i, track in enumerate(tracks_lisp):
-            end_index = End_index[i]
-            indicator_silence = -1
-            for j, note in enumerate(track):
-                if note[0] == 'silence':
-                    NoteOnEvent = midi.events.NoteOnEvent(tick=note[1], channel=i, data=[track[j+1][0], 80])
-                    NoteOffEvent = midi.events.NoteOffEvent(tick=track[j+1][1], channel=i, data=[track[j+1][0], 0])
-                    pattern_out[i+1].insert(-end_index,NoteOnEvent)
-                    pattern_out[i+1].insert(-end_index,NoteOffEvent)
-                    indicator_silence = j+1
-                if (note[0] != 'silence') & (indicator_silence != j):
-                    NoteOnEvent = midi.events.NoteOnEvent(tick=0, channel=i, data=[note[0], 80])
-                    NoteOffEvent = midi.events.NoteOffEvent(tick=note[1], channel=i, data=[note[0], 0])
-                    pattern_out[i+1].insert(-end_index,NoteOnEvent)
-                    pattern_out[i+1].insert(-end_index,NoteOffEvent)
+                    # Add generated notes to track
+                    lispTrack += notes
+            self.lispTracks.append(lispTrack)
 
-        return pattern_out
+    def write(self, lispTracks=None):
+        # copy pattern metadata
+        midiPattern            = midi.containers.Pattern()
+        midiPattern.format     = self.midiPattern.format
+        midiPattern.resolution = self.midiPattern.resolution
+        midiPattern.insert(2, self.midiPattern[0])
 
-    def Midi_to_Lisp(self, pattern):
-        tracks = []
-        for i, track in enumerate(pattern): # Enumerate all the tracks
-            if i > 0: # Don't take into account the first track of metadata
-                liste_track = [] # Create an empty list for the output desired track in the right format
-                for j, event in enumerate(track):
-                    if type(event) == midi.events.NoteOnEvent:
-                        if type(track[j+1]) == midi.events.NoteOffEvent: # Get (pitch, duration) for notes that are not silence
-                            if event.tick == 0:
-                                pitch = event.data[0]
-                                duration = track[j+1].tick - event.tick
-                                note = (pitch,duration)
-                                liste_track.append(note)
-                            else: # If there is a silence, then we need to extract both the silence and the following note
-                                pitch = 'silence' # extract the silence and its duration
-                                duration = event.tick
-                                silence = (pitch, duration)
-                                liste_track.append(silence)
-                                pitch = track[j+1].data[0] # extract the following note
-                                duration = track[j+1].tick
-                                note = (pitch,duration)
-                                liste_track.append(note)
-                tracks.append(liste_track)
-        return tracks
+        # Create tracks
+        if lispTracks is None: lispTracks = self.lispTracks
+        midiTracks = []
+        for _ in range(len(lispTracks)):
+            midiPattern.insert(2, midi.containers.Track())
+            midiTracks.append([])
 
-class BachChoraleTrack(BasicTrack):
+        # Generate tracks' NoteOnEvents and NoteOffEvents
+        for lispTrackId, lispTrack in enumerate(lispTracks):
+            noteNumAfterSilence = -1
+            for noteNum, note in enumerate(lispTrack):
+                if note[0] == "silence":
+                    NoteOnEvent = midi.events.NoteOnEvent(tick=note[1], \
+                                                          channel=lispTrackId, \
+                                                          data=[lispTrack[noteNum+1][0], 80])
+
+                    NoteOffEvent = midi.events.NoteOffEvent(tick=lispTrack[noteNum+1][1], \
+                                                            channel=lispTrackId, \
+                                                            data=[lispTrackId[noteNum+1][0], 0])
+
+                    midiTracks[lispTrackId] += [NoteOnEvent, NoteOffEvent]
+                    noteNumAfterSilence = noteNum+1
+
+                elif noteNum != noteNumAfterSilence:
+                    NoteOnEvent = midi.events.NoteOnEvent(tick=0, \
+                                                          channel=lispTrackId, \
+                                                          data=[note[0], 80])
+                    NoteOffEvent = midi.events.NoteOffEvent(tick=note[1], \
+                                                            channel=lispTrackId, \
+                                                            data=[note[0], 0])
+                    midiTracks[lispTrackId] += [NoteOnEvent, NoteOffEvent]
+
+        # Add tracks to midiPattern
+        for midiTrackId, midiTrack in enumerate(self.midiPattern):
+            if midiTrackId == 0: continue
+            for eventNum, event in enumerate(midiTrack):
+                if type(event) not in [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
+                    midiPattern[midiTrackId].append(event)
+                elif type(midiTrack[eventNum-1]) not in \
+                [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
+                    midiPattern[midiTrackId].extend(midiTracks[midiTrackId-1])
+
+        print(midiPattern)
+        print(self.midiPattern)
+        # Write file
+        i = 1
+        while os.path.isfile(self.midiFile[0:-4] + str(i) + ".mid"):
+            i += 1
+        fileName = self.midiFile[0:-4] + str(i) + ".mid"
+        midi.write_midifile(fileName, midiPattern)
+        print "Wrote %s" % fileName
+
+
+class LispBachChorale(MusicPattern):
     """
     This class represents a choral from the Bach dataset
     (https://archive.ics.uci.edu/ml/datasets/Bach+Chorales)
@@ -110,7 +116,8 @@ class BachChoraleTrack(BasicTrack):
         self.choraleFile = choraleFile
         self.choraleNum  = ((int(choraleNum)-1) % 100) + 1
         self.features    = ["pitch", "dur"]
-        self.notes       = []
+        self.numTracks   = 1
+        self.lispTrack   = []
 
         with open(self.choraleFile, 'r') as f:
             line = f.readlines()[2*(self.choraleNum-1)]
@@ -128,7 +135,7 @@ class BachChoraleTrack(BasicTrack):
 
                     featurePosition = line.find(self.features[(i+1) % N], valueEndPosition)
                     valueEndPosition = line.find(')', featurePosition)
-                self.notes += [tuple(note)]
+                self.lispTrack += [tuple(note)]
 
     def __repr__(self):
         """
@@ -137,10 +144,12 @@ class BachChoraleTrack(BasicTrack):
         ret = "Bach Chorale %d (line %d in file %s)\n" % \
         (self.choraleId, 2*self.choraleNum + 1, self.choraleFile)
         ret += "pitch   dur\n"
-        for note in self.notes:
+        for note in self.lispTrack:
             ret += "   %2d    %2d\n" % note
         return ret
 
+
 if __name__ == '__main__':
-    myChorale = BachChoraleTrack("../dat/bach-chorales/chorales.lisp", 2)
-    print(myChorale)
+    myOde = MusicPattern("../../test/ode.mid")
+    myOde.lispTracks[0][0] = (77, 960)
+    myOde.write()
