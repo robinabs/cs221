7f66db9d0a153b4996659a24b0691ae68e059bb5
Charles Burlin <cburlin@stanford.edu>
2017-11-15 09:57:11 -0800

    add formatToOtherformat methods to MusicPattern class

--- src/dataUtil.py
+++ src/dataUtil.py
@@ -1,3 +1,4 @@
+from fractions import gcd
 import midi
 import os.path
 
@@ -16,7 +17,15 @@ class MusicPattern():
         self.numTracks   = len(self.midiPattern)-1
         self.lispTracks  = []
 
-        for midiTrackId, midiTrack in enumerate(self.midiPattern):
+        # Do midi -> lisp conversion
+        self.lispTracks = self.midiToLisp()
+        # Do lisp -> prim conversion
+        self.primTracks, self.rythm_unit = self.lispToPrim()
+
+    def midiToLisp(self, midiPattern=None):
+        if midiPattern is None: midiPattern = self.midiPattern
+        lispTracks = []
+        for midiTrackId, midiTrack in enumerate(midiPattern):
             if midiTrackId == 0: continue
 
             lispTrack = []
@@ -38,14 +47,52 @@ class MusicPattern():
 
                     # Add generated notes to track
                     lispTrack += notes
-            self.lispTracks.append(lispTrack)
+            lispTracks.append(lispTrack)
+        return lispTracks
 
-    def write(self, lispTracks=None):
-        # copy pattern metadata
-        midiPattern            = midi.containers.Pattern()
-        midiPattern.format     = self.midiPattern.format
-        midiPattern.resolution = self.midiPattern.resolution
-        midiPattern.insert(2, self.midiPattern[0])
+    def lispToPrim(self, lispTracks=None):
+        if lispTracks is None: lispTracks = self.lispTracks
+        primTracks = []
+        for lispTrack in lispTracks:
+            min_duration = 0
+            # @robin: a quoi sert ce if ?
+            if lispTrack:
+                durations = [lispTrack[i][1] for i in range(len(lispTrack))]
+                rythm_unit = reduce(gcd, durations)
+                primTrack = []
+                for note in lispTrack:
+                    assert(note[1] % rythm_unit == 0)
+                    primTrack += [(note[0],True) if (i < note[1]/rythm_unit - 1) else (note[0],False) \
+                                  for note in lispTrack for i in range(note[1]/rythm_unit)]
+                primTracks.append(primTrack)
+        return primTracks, rythm_unit
+
+    def primToLisp(self, primTracks=None):
+        if primTracks is None: primTracks = self.primTracks
+        lispTracks = []
+        for primTrack in primTracks:
+            # @robin: a quoi sert ce if ?
+            lispTrack = []
+            if primTrack:
+                i = 0
+                while i < len(primTrack):
+                    note = [primTrack[i][0], self.rythm_unit]
+                    while primTrack[i][1]:
+                        note[1] += self.rythm_unit
+                        i += 1
+                    i+=1
+                    lispTrack.append(tuple(note))
+            lispTracks.append(lispTrack)
+
+        return lispTracks
+
+    def write(self, fileName=None, lispTracks=None):
+        midiPattern = midi.containers.Pattern()
+        if self.midiPattern is not None:
+            # copy pattern metadata
+            midiPattern.format     = self.midiPattern.format
+            midiPattern.resolution = self.midiPattern.resolution
+            midiPattern.insert(2, self.midiPattern[0])
 
         # Create tracks
         if lispTracks is None: lispTracks = self.lispTracks
@@ -80,27 +127,33 @@ class MusicPattern():
                     midiTracks[lispTrackId] += [NoteOnEvent, NoteOffEvent]
 
         # Add tracks to midiPattern
-        for midiTrackId, midiTrack in enumerate(self.midiPattern):
-            if midiTrackId == 0: continue
-            for eventNum, event in enumerate(midiTrack):
-                if type(event) not in [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
-                    midiPattern[midiTrackId].append(event)
-                elif type(midiTrack[eventNum-1]) not in \
-                [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
-                    midiPattern[midiTrackId].extend(midiTracks[midiTrackId-1])
+        if self.midiPattern is not None:
+            for midiTrackId, midiTrack in enumerate(self.midiPattern):
+                if midiTrackId == 0: continue
+                for eventNum, event in enumerate(midiTrack):
+                    if type(event) not in [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
+                        midiPattern[midiTrackId].append(event)
+                    elif type(midiTrack[eventNum-1]) not in \
+                    [midi.events.NoteOnEvent, midi.events.NoteOffEvent]:
+                        midiPattern[midiTrackId].extend(midiTracks[midiTrackId-1])
+        else:
+            for midiTrackId, midiTrack in enumerate(midiTracks):
+                midiPattern[midiTrackId].extend(midiTracks[midiTrackId])
+            midiPattern[0].append(midi.EndOfTrackEvent(tick=1))
 
-        print(midiPattern)
-        print(self.midiPattern)
+
         # Write file
-        i = 1
-        while os.path.isfile(self.midiFile[0:-4] + str(i) + ".mid"):
-            i += 1
-        fileName = self.midiFile[0:-4] + str(i) + ".mid"
+        if fileName is None:
+            i = 1
+            while os.path.isfile(self.midiFile[0:-4] + str(i) + ".mid"):
+                i += 1
+            fileName = self.midiFile[0:-4] + str(i) + ".mid"
+
         midi.write_midifile(fileName, midiPattern)
         print "Wrote %s" % fileName
 
 
-class LispBachChorale(MusicPattern):
+class BachChorale(MusicPattern):
     """
     This class represents a choral from the Bach dataset
     (https://archive.ics.uci.edu/ml/datasets/Bach+Chorales)
@@ -117,7 +170,8 @@ class LispBachChorale(MusicPattern):
         self.choraleNum  = ((int(choraleNum)-1) % 100) + 1
         self.features    = ["pitch", "dur"]
         self.numTracks   = 1
-        self.lispTrack   = []
+        self.lispTracks  = [[]]
+        self.midiPattern = None
 
         with open(self.choraleFile, 'r') as f:
             line = f.readlines()[2*(self.choraleNum-1)]
@@ -131,11 +185,12 @@ class LispBachChorale(MusicPattern):
                 note = []
                 for i, feature in enumerate(self.features):
                     val = int(line[(featurePosition+len(feature)+1):valueEndPosition])
+                    if feature == "dur": val *= 64
                     note += [val]
 
                     featurePosition = line.find(self.features[(i+1) % N], valueEndPosition)
                     valueEndPosition = line.find(')', featurePosition)
-                self.lispTrack += [tuple(note)]
+                self.lispTracks[0] += [tuple(note)]
 
     def __repr__(self):
         """
@@ -144,12 +199,13 @@ class LispBachChorale(MusicPattern):
         ret = "Bach Chorale %d (line %d in file %s)\n" % \
         (self.choraleId, 2*self.choraleNum + 1, self.choraleFile)
         ret += "pitch   dur\n"
-        for note in self.lispTrack:
+        for note in self.lispTracks[0]:
             ret += "   %2d    %2d\n" % note
         return ret
 
 
 if __name__ == '__main__':
+    #myChorale = BachChorale("../dat/bach-chorales/chorales.lisp", 5)
+    #myChorale.write("../dat/bach-chorales/mid/chorale5.mid")
     myOde = MusicPattern("../../test/ode.mid")
-    myOde.lispTracks[0][0] = (77, 960)
-    myOde.write()
+    myOde.primToLisp()
